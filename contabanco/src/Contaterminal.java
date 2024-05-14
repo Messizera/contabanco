24a267a42347f76b2541ae797b6a51b955ebbe5a
Lucas Rodrigues <lucasestudosmessi@gmail.com>
2024-05-14 20:46:54 -0300

    meu banco

--- contabanco/src/Contaterminal.java
+++ contabanco/src/Contaterminal.java
@@ -4,7 +4,6 @@ import java.util.Scanner;
 public class Contaterminal {
     public static void main(String[] args) {
         int saldo = 0;
-        //criando o objeto scanner
         Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
         
         System.out.println("Digite seu Nome");
