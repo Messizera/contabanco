b41b557fe13bd241c3e181d7e76d8f28ac3f7d0c
Lucas Rodrigues <lucasestudosmessi@gmail.com>
2024-05-15 20:26:05 -0300

    banco top

--- contabanco/src/Contaterminal.java
+++ contabanco/src/Contaterminal.java
@@ -19,7 +19,37 @@ public class Contaterminal {
 
         
         System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque");
+        
+        System.out.println("Digite 1 - para realizar um deposito");
+        System.out.println("Digite 2 - para realizar um saque");
+        System.out.println("Digite 3 - para sair");
+        int depositar = scanner.nextInt();
+
+        if ( depositar == 1) {
+            System.out.print("Digite o Valor a ser depositado: ");
+            int valordeposito = scanner.nextInt();
+    
+        saldo = saldo + valordeposito;
+        System.out.print( "seu saldo atual é de " + saldo + " reais");
+        }
+        
+        if ( depositar == 2){
+            System.out.print("Digite o Valor de saque: ");
+            int valorsaque = scanner.nextInt();
+                
+            if (saldo > valorsaque){
+            saldo = saldo - valorsaque;
+            System.out.print( "seu saldo atual é de " + saldo + " reais");}
+            else
+            System.out.print( "Saldo insuficiente");}
+            
+        if ( depositar == 3){
+            System.out.print("Obrigado por utilizar o banco");
+        }
+
+        }
+
     }
         
-        
-    }
+    
+    
